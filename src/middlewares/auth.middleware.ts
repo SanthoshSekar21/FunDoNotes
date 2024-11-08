30b9a6dd5ba95917e5072d17c70e73c9f9f46495
SanthoshSekar21 <santhosh20sekar@gmail.com>
2024-11-08 13:20:45 +0530

    [Santhosh] added: cache for getAllNotes by using redis

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -1,40 +1,35 @@
- /* eslint-disable @typescript-eslint/no-explicit-any */
+//  /* eslint-disable @typescript-eslint/no-explicit-any */
 import HttpStatus from 'http-status-codes';
-import jwt from 'jsonwebtoken'
+import jwt  from 'jsonwebtoken';
 import { Request, Response, NextFunction } from 'express';
-
 
 /**
  * Middleware to authenticate if user has a valid Authorization token
  * Authorization: Bearer <token>
  *
- * @param {Object} req
- * @param {Object} res
- * @param {Function} next
+ * @param {string} secret - The secret key used for JWT verification
  */
-export const userAuth = async (
-  req: Request,
-  res: Response,
-  next: NextFunction
-): Promise<void> => {
-  try {
-    const authHeader = req.header('Authorization');
-    const bearerToken = authHeader?.split(' ')[1];
-    if (!bearerToken)
-      throw {
-        code: HttpStatus.BAD_REQUEST,
-        message: 'Authorization token is required'
-      };try{
-    const user: any = await jwt.verify(bearerToken, process.env.JWT_SECRET);
-    req.body.createdBy=user.id;
-      }catch(error){
-        req.body.email =(await jwt.verify(bearerToken,process.env.JWT_FORGETSECRET)).Email;
-    
+export const userAuth = (secret: string) => {
+  return async (req: Request, res: Response, next: NextFunction): Promise<any> => {
+    try {
+      const authHeader = req.header('Authorization');
+      if (!authHeader) {
+        throw new Error('Authorization token is required')
       }
-    // res.locals = user;
-    // res.locals.token = bearerToken;
-    next();
-  } catch (error) {
-    next(error);
-  }
+      const bearerToken = authHeader.split(' ')[1];
+      if (!bearerToken) {
+        return next({
+            code: HttpStatus.BAD_REQUEST,
+             message: 'Authorization token is required',
+            });
+      }
+      const user: any = await jwt.verify(bearerToken, secret);
+      req.body.createdBy = user.id;
+      req.body.Email = user.Email;
+
+      next();
+    } catch (error) {
+      next(error);
+    } 
+  };
 };
--- src/routes/note.route.ts
+++ src/routes/note.route.ts
@@ -2,7 +2,9 @@ import express, { IRouter } from 'express';
 import NoteController from '../controllers/note.controller';
 import NoteValidator from '../validators/note.validator';
  import { userAuth } from '../middlewares/auth.middleware';
- 
+ import { cache } from '../middlewares/cache.middleware';
+ import dotenv from 'dotenv';
+dotenv.config();
 class NoteRoutes {
   private NoteController = new NoteController();
   private router = express.Router();
@@ -15,38 +17,58 @@ class NoteRoutes {
   private routes = () => {
     // //route to get all users
     // this.router.get('', this.UserController.getAllUsers);
-
+    
    
     this.router.post('/createnote', 
       this.NoteValidator.newNote,
-      userAuth,
+      userAuth(process.env.JWT_SECRET),
       this.NoteController.createNote);
+   
+      this.router.get('/viewall',
+       userAuth(process.env.JWT_SECRET), cache,
+       this.NoteController.getAllNotes);
 
-      this.router.get('/viewall', userAuth, this.NoteController.getAllNotes);
     // //route to get a single note
-    this.router.get('/viewone/:noteId', userAuth, this.NoteController.getNote);
+     this.router.get('/viewone/:noteId', 
+      userAuth(process.env.JWT_SECRET), 
+      this.NoteController.getNote);
 
     // //route to update a note
-    this.router.put('/update/:noteId', userAuth, this.NoteController.updateNote );
+    this.router.put('/update/:noteId', 
+      userAuth(process.env.JWT_SECRET), 
+      this.NoteController.updateNote );
 
     //route to delete a note
-    this.router.delete('/delete/:noteId',userAuth, this.NoteController.deleteNote);
+    this.router.delete('/delete/:noteId',
+      userAuth(process.env.JWT_SECRET), 
+      this.NoteController.deleteNote);
+
     //route to a trash
 
-    this.router.delete('/trash/:noteId',userAuth,this.NoteController.trash);
-    this.router.put('/trash/:noteId',userAuth,this.NoteController.trash);
+
+    this.router.put('/trash/:noteId',
+      userAuth(process.env.JWT_SECRET),
+      this.NoteController.trash);
 
     //route to view the trash note
-    this.router.get('/trash',userAuth,this.NoteController.viewTrash);
+    this.router.get('/trash',
+      userAuth(process.env.JWT_SECRET),
+      this.NoteController.viewTrash);
 
     // route to a archeive
-    this.router.put('/archive/:noteId',userAuth,this.NoteController.archive);
+    this.router.put('/archive/:noteId',
+      userAuth(process.env.JWT_SECRET),
+      this.NoteController.archive);
 
     //route to a view the archive note
-    this.router.get('/archive',userAuth,this.NoteController.viewArchive);
+    this.router.get('/archive',
+      userAuth(process.env.JWT_SECRET),
+      this.NoteController.viewArchive);
 
     //route to delete permanently
-    this.router.delete('/permanentlydelete/:noteId',userAuth,this.NoteController.PermanentlyDelete);
+    this.router.delete('/permanentlydelete/:noteId',
+      userAuth(process.env.JWT_SECRET),
+      this.NoteController.PermanentlyDelete);
   };
 
   public getRoutes = (): IRouter => {
